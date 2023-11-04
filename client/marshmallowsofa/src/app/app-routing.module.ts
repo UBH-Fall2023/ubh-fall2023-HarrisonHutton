11abe29d542171fa02ea470968baee0999c23355
HarrisonHutton <harrison.hutton6@gmail.com>
2023-11-04 17:23:24 -0400

    ✨ Create room ids

--- client/marshmallowsofa/src/app/app-routing.module.ts
+++ client/marshmallowsofa/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { CreateLobbyComponent } from './create-lobby/create-lobby.component';
 
 const routes: Routes = [
-  { path: '', component: AppLayoutComponent }
+  { path: '', component: AppLayoutComponent, children: [
+    { path: '', component: CreateLobbyComponent }
+  ]}
 ];
 
 @NgModule({
--- client/marshmallowsofa/src/app/app.module.ts
+++ client/marshmallowsofa/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { CreateLobbyComponent } from './create-lobby/create-lobby.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppLayoutComponent
+    AppLayoutComponent,
+    CreateLobbyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
