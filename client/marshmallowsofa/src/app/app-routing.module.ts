8720f9287bff45865e0ce47ca728c4fb17205e16
HarrisonHutton <harrison.hutton6@gmail.com>
2023-11-04 15:56:52 -0400

    ✨ Create main page

---
+++ client/marshmallowsofa/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+
+const routes: Routes = [
+  { path: '', component: AppLayoutComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
